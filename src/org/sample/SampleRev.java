d10445be82f05c2653628b3c56b98b096be6da37
RamkumarV09 <ramkumarv0986@gmail.com>
2024-11-10 10:02:18 +0530

    New Code

---
+++ src/org/sample/SampleRev.java
@@ -0,0 +1,26 @@
+package org.sample;
+
+public class SampleRev {
+	
+	public void meth1() {
+		
+		System.out.println("Meth 1");
+	}
+	
+	public void meth2() {
+		
+		System.out.println("Meth 2");
+
+	}
+	
+	public void meth3() {
+		System.out.println("Meth 3");
+
+	}
+	
+	public void meth4() {
+		System.out.println("Meth4");
+
+	}
+
+}
